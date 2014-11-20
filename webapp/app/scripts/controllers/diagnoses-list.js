6636f78d239d97c9a57368d722a37a8ce33ab383
gforbes <glenn.forbes@kikaikintou.com>
2014-11-20 10:02:36 +0000

    An example of paging/filtering/ordering using Angular $filter.  The tipping point for moving this from client-side to server-side would need to be investigated based on the amount of diagnoses data.
    An alternative would be the DataTables jquery plugin.
    
    If this approach is used, it will need the 'unobtrusive diagnosis refresh' functionality to be implemented too - otherwise, when a diagnosis is selected, $location.path(path) will reload the controller and reset any paging, filtering, ordering etc.

--- webapp/app/scripts/controllers/diagnoses-list.js
+++ webapp/app/scripts/controllers/diagnoses-list.js
@@ -8,12 +8,56 @@
  * Controller of the openehrPocApp
  */
 angular.module('openehrPocApp')
-  .controller('DiagnosesListCtrl', function ($scope, $stateParams, $location, $modal, Patient, Diagnosis, growlNotifications) {
+  .controller('DiagnosesListCtrl', function ($scope, $stateParams, $location, $modal, Patient, Diagnosis, growlNotifications, $filter) {
     $scope.patient = Patient.get($stateParams.patientId);
+
+    $scope.numPerPageOpt = [3, 5, 10, 20];
+    $scope.numPerPage = $scope.numPerPageOpt[2];
+    $scope.currentPage = 1;
+    $scope.currentPageDiagnoses = [];
+
+    $scope.searchKeywords = '';
+    $scope.filteredDiagnoses = [];
+    $scope.defaultOrder = '-dateOfOnset';
+    $scope.row = $scope.defaultOrder;
 
     Diagnosis.byPatient($scope.patient.id).then(function (result) {
       $scope.patient.diagnoses = result;
+      $scope.search();
     });
+
+    $scope.select = function(page) {
+      var end, start;
+      start = (page - 1) * $scope.numPerPage;
+      end = start + $scope.numPerPage;
+      return $scope.currentPageDiagnoses = $scope.filteredDiagnoses.slice(start, end);
+    };
+    $scope.onFilterChange = function() {
+      $scope.select(1);
+      $scope.currentPage = 1;
+      return $scope.row = $scope.defaultOrder;
+    };
+    $scope.onNumPerPageChange = function() {
+      $scope.select(1);
+      return $scope.currentPage = 1;
+    };
+    $scope.onOrderChange = function() {
+      $scope.select(1);
+      return $scope.currentPage = 1;
+    };
+    $scope.search = function() {
+      $scope.row = $scope.defaultOrder;
+      $scope.filteredDiagnoses = $filter('orderBy')($filter('filter')($scope.patient.diagnoses, $scope.searchKeywords), $scope.row);
+      return $scope.onFilterChange();
+    };
+    $scope.order = function(rowName) {
+      if ($scope.row === rowName) {
+        return;
+      }
+      $scope.row = rowName;
+      $scope.filteredDiagnoses = $filter('orderBy')($filter('filter')($scope.patient.diagnoses, $scope.searchKeywords), $scope.row);
+      return $scope.onOrderChange();
+    };
 
     $scope.go = function (path) {
       $location.path(path);
